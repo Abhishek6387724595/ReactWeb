909e08b62f7f12613741c3d2cc8685550ab0bad2
Abhishek Awasthi <avasthiabhishek0033@gmail.com>
2020-10-20 23:35:01 +0530

    Updates

---
+++ precache-manifest.c804153a1ff3fdd0a64c5ad0ab8ca9e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f60a28472ed9305115d97d85ac73ff",
+    "url": "/ReactWeb/index.html"
+  },
+  {
+    "revision": "7753c3c2de00c107f758",
+    "url": "/ReactWeb/static/css/2.d04fdd5c.chunk.css"
+  },
+  {
+    "revision": "65e50fbd2c1f2bcf0149",
+    "url": "/ReactWeb/static/css/main.25e8e176.chunk.css"
+  },
+  {
+    "revision": "7753c3c2de00c107f758",
+    "url": "/ReactWeb/static/js/2.7f64acef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactWeb/static/js/2.7f64acef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e50fbd2c1f2bcf0149",
+    "url": "/ReactWeb/static/js/main.ed77bd43.chunk.js"
+  },
+  {
+    "revision": "53cf7b9cecd538d57992",
+    "url": "/ReactWeb/static/js/runtime-main.0c49e1bd.js"
+  },
+  {
+    "revision": "b5e47c10ffd5ad709c749c4a285ab47c",
+    "url": "/ReactWeb/static/media/about.b5e47c10.jpg"
+  },
+  {
+    "revision": "b67c2ee56cc74c75db66491ce1367263",
+    "url": "/ReactWeb/static/media/cherr.b67c2ee5.jpg"
+  },
+  {
+    "revision": "b76e4628daa21dfa2feedf7ee0a12651",
+    "url": "/ReactWeb/static/media/moon.b76e4628.jpg"
+  },
+  {
+    "revision": "cdf0ec04ad2b59fcaadd5ef6b35b8c61",
+    "url": "/ReactWeb/static/media/moonblue.cdf0ec04.jpg"
+  },
+  {
+    "revision": "b78e0338d43009e61b0f7ce6fad74c21",
+    "url": "/ReactWeb/static/media/pinktree.b78e0338.jpg"
+  },
+  {
+    "revision": "2a0872c45c054306e3118eb1c46a7af5",
+    "url": "/ReactWeb/static/media/saasimage.2a0872c4.jpg"
+  },
+  {
+    "revision": "c1282942647e40314ced2df3600ef658",
+    "url": "/ReactWeb/static/media/tree.c1282942.jpg"
+  },
+  {
+    "revision": "c7aaf5659a3bbcdd10f07d2e25694034",
+    "url": "/ReactWeb/static/media/treemoon.c7aaf565.jpg"
+  }
+]);
